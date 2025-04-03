a14aa8a97a9ec0cb1c529ae154ebffacced2e84d
Braeden Turner <braedenturner22@gmail.com>
2025-04-02 21:10:00 -0400

    adding object name funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
-import { ImageCropperComponent} from "../image-cropper/image-cropper.component";
+import { Component } from "@angular/core";
+import { ImageCropperComponent } from "../image-cropper/image-cropper.component";
 import { ImageLabelsComponent } from "../image-labels/image-labels.component";
-import { ObjectCreationFormComponent } from '../object-creation-form/object-creation-form.component';
-import { FormIsFinishedService } from '../form-finished.service';
+import { ObjectCreationFormComponent } from "../object-creation-form/object-creation-form.component";
+import { FormIsFinishedService } from "../form-finished.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [ImageCropperComponent, ImageLabelsComponent, ObjectCreationFormComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    ImageCropperComponent,
+    ImageLabelsComponent,
+    ObjectCreationFormComponent,
+    CommonModule,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-
   objectCreationFormFinished: boolean | null = false;
 
   constructor(private FormIsFinishedService: FormIsFinishedService) {}
 
-ngOnInit(): void {
-  this.FormIsFinishedService.formOpen$.subscribe(data => {
-    this.objectCreationFormFinished = data;
-  });
-}
+  ngOnInit(): void {
+    this.FormIsFinishedService.formOpen$.subscribe((data) => {
+      this.objectCreationFormFinished = data;
+    });
+  }
 }
--- src/object-creation-form/object-creation-form.component.ts
+++ src/object-creation-form/object-creation-form.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FormIsFinishedService } from '../form-finished.service';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { FormIsFinishedService } from "../form-finished.service";
+import { throwError } from "rxjs";
 
 @Component({
-  selector: 'app-object-creation-form',
+  selector: "app-object-creation-form",
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './object-creation-form.component.html',
-  styleUrl: './object-creation-form.component.css'
+  templateUrl: "./object-creation-form.component.html",
+  styleUrl: "./object-creation-form.component.css",
 })
 export class ObjectCreationFormComponent implements OnInit {
-
+  newProjectCreated: boolean = false;
   objectFormIsOpen: boolean = false;
   formIsFinished: boolean = false;
-  objectDict: { [key: number]: string} = {};
+  objectDict: { [key: number]: string } = {};
+  errorMessage: string = "";
 
-  newObjectClass: string = '';
-  
+  newObjectClass: string = "";
+
   nextKey: number = 0;
 
   constructor(private FormIsFinishedService: FormIsFinishedService) {}
@@ -25,14 +27,25 @@ export class ObjectCreationFormComponent implements OnInit {
   ngOnInit(): void {}
 
   initiateObjectForm() {
+    this.newProjectCreated = true;
     this.objectFormIsOpen = true;
   }
 
   addObject() {
     if (this.newObjectClass.trim()) {
-      this.objectDict[this.nextKey] = this.newObjectClass;
-      this.nextKey++;
-      this.newObjectClass = '';
+      const valueExists = Object.values(this.objectDict).includes(
+        this.newObjectClass
+      );
+      if (!valueExists) {
+        this.objectDict[this.nextKey] = this.newObjectClass;
+        this.nextKey++;
+        this.newObjectClass = "";
+      } else {
+        this.errorMessage =
+          "Class name already exists, please pick a different name";
+      }
+    } else {
+      this.errorMessage = "Class name cannot be empty, please pick a name";
     }
   }
 
@@ -42,14 +55,12 @@ export class ObjectCreationFormComponent implements OnInit {
 
   // Get an array of keys for *ngFor iteration
   getKeys(dict: { [key: number]: string }): number[] {
-    return Object.keys(dict).map(key => parseInt(key, 10));
+    return Object.keys(dict).map((key) => parseInt(key, 10));
   }
 
   finishForm() {
     this.formIsFinished = true;
     this.FormIsFinishedService.updateFormStatus(this.formIsFinished);
+    this.objectFormIsOpen = false;
   }
-
 }
-
-
